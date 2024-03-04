1aa3fae402b0ef6684b7fa8d7defbdf31315cb7f
nainish <pmo_dbalounge@shubham.co>
2024-03-05 00:07:44 +0530

    create response page

---
+++ src/main/java/com/enach/Controller/ResponseController.java
@@ -0,0 +1,18 @@
+package com.enach.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class ResponseController {
+
+    @RequestMapping("/response")
+    public String getData()
+    {
+        return "response";
+    }
+}
+
