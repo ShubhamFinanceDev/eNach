3a0fd211a5adbeeefdc0ef6bf0310ea450bd6d4c
nainish <pmo_dbalounge@shubham.co>
2024-03-05 00:43:18 +0530

    create response page

--- src/main/java/com/enach/Controller/CustomerController.java
+++ src/main/java/com/enach/Controller/CustomerController.java
@@ -23,11 +23,7 @@ public class CustomerController {
 @Autowired
 private CoustomerService coustomerService;
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String demo(){
 
-        return "Hello programmer";
-    }
     @PostMapping("/sendOtp")
     public HashMap sendOtpOnCustomerRegisteredMobile(@RequestBody Map<String, String> inputParam) {
 
