0b453fcbd1d0d48e431abed4c44f889a56aafacb
nainish <pmo_dbalounge@shubham.co>
2024-03-05 00:50:11 +0530

    create response page

--- src/main/java/com/enach/Controller/CustomerController.java
+++ src/main/java/com/enach/Controller/CustomerController.java
@@ -23,7 +23,11 @@ public class CustomerController {
 @Autowired
 private CoustomerService coustomerService;
 
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String demo(){
 
+        return "Hello programmer";
+    }
     @PostMapping("/sendOtp")
     public HashMap sendOtpOnCustomerRegisteredMobile(@RequestBody Map<String, String> inputParam) {
 
@@ -96,6 +100,10 @@ private CoustomerService coustomerService;
         }
     }
 
-
+    @RequestMapping("/response")
+    public String getData()
+    {
+        return "response";
+    }
 
 }
--- src/main/java/com/enach/Controller/ResponseController.java
+++ src/main/java/com/enach/Controller/ResponseController.java
@@ -1,22 +1,22 @@
-package com.enach.Controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
-@Controller
-@RequestMapping("/eNach")
-
-public class ResponseController {
-
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String demo(){
-
-        return "Hello programmer";
-    }
-    @RequestMapping("/response")
-    public String getData()
-    {
-        return "response";
-    }
-}
-
+//package com.enach.Controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.*;
+//
+//@Controller
+//@RequestMapping("/eNach")
+//
+//public class ResponseController {
+//
+//    @RequestMapping(value = "/", method = RequestMethod.GET)
+//    public String demo(){
+//
+//        return "Hello programmer";
+//    }
+//    @RequestMapping("/response")
+//    public String getData()
+//    {
+//        return "response";
+//    }
+//}
+//
