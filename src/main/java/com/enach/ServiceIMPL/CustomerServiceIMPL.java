28ea4ddec7899d7caff856b9d8c887fc3e995958
ShubhamFinanceDev <pmo_dbalounge@shubham.co>
2024-07-11 21:30:30 +0530

    static phone no for testing

--- src/main/java/com/enach/ServiceIMPL/CustomerServiceIMPL.java
+++ src/main/java/com/enach/ServiceIMPL/CustomerServiceIMPL.java
@@ -64,8 +64,8 @@ public class CustomerServiceIMPL implements CoustomerService {
                     if (otpCode > 0) {
 
                         System.out.println("otp generated successfully");
-//                        if (otpUtility.sendOtp(customerDetails.getPhoneNumber(), otpCode, customerDetails.getLoanAccountNo())) {
-                        if (otpUtility.sendOtp("8160041657", otpCode, customerDetails.getLoanAccountNo())) {
+                        if (otpUtility.sendOtp(customerDetails.getPhoneNumber(), otpCode, customerDetails.getLoanAccountNo())) {
+//                        if (otpUtility.sendOtp("8160041657", otpCode, customerDetails.getLoanAccountNo())) {
 
                             System.out.println("otp sent on mobile");
                             OtpDetails otpDetails = new OtpDetails();
