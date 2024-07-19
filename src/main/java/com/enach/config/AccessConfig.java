8a677e8b810fd6f86d24a9d156fe784a90362dbc
ShubhamFinanceDev <pmo_dbalounge@shubham.co>
2024-07-19 17:37:10 +0530

    cors fix

--- src/main/java/com/enach/config/AccessConfig.java
+++ src/main/java/com/enach/config/AccessConfig.java
@@ -2,9 +2,10 @@ package com.enach.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+@Configuration
 public class AccessConfig {
     @Value("${access_cors_origin}")
     private String accessCorsOrigin;
