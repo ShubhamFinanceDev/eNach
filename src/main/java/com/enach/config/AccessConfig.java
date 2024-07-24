0ca1d27eb3a751c715cce40cc3dcb11a046f9372
ShubhamFinanceDev <pmo_dbalounge@shubham.co>
2024-07-24 22:36:20 +0530

    fix mandate type

--- src/main/java/com/enach/config/AccessConfig.java
+++ src/main/java/com/enach/config/AccessConfig.java
@@ -7,20 +7,20 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class AccessConfig {
-//    @Value("${access_cors_origin}")
-//    private String accessCorsOrigin;
-//    @Value("${allow_credentials}")
-//    private Boolean allowCredentials;
+    @Value("${access_cors_origin}")
+    private String accessCorsOrigin;
+    @Value("${allow_credentials}")
+    private Boolean allowCredentials;
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-//                        .allowedOrigins(accessCorsOrigin)
+                        .allowedOrigins(accessCorsOrigin)
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*");
-//                        .allowCredentials(allowCredentials);
+                        .allowedHeaders("*")
+                        .allowCredentials(allowCredentials);
             }
         };
     }
