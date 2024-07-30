8099812d2c037323c3620b21e90f5b22a5c8fe83
nainish <pmo_dbalounge@shubham.co>
2024-07-30 10:41:32 +0530

    fix cron job

--- src/main/java/com/enach/ServiceIMPL/CustomerServiceIMPL.java
+++ src/main/java/com/enach/ServiceIMPL/CustomerServiceIMPL.java
@@ -218,7 +218,7 @@ public class CustomerServiceIMPL implements CoustomerService {
     }
 
     @Override
-    @Scheduled(cron = "30 * * * * *") // 30 minutes
+    @Scheduled(cron = "* 30 * * * *") // 30 minutes
     public void generateReportOnMail() {
         logger.info("Generating report on mail process invoke at {}", LocalDateTime.now());
         try {
