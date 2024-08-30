8d079e1c57d51c1d70ab26ea339d1b1085e2248a
shagundba <shagun.kaushik@dbalounge.com>
2024-08-30 12:52:39 +0530

    improvements

--- src/main/java/com/enach/config/SecurityConfig.java
+++ src/main/java/com/enach/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
                 csrf(csrf->csrf.disable())
 
 //                .cors(cors->cors.disable())
-                .authorizeHttpRequests(auth ->auth.requestMatchers("/customer/**").authenticated().requestMatchers("/cancel/otpVerification").permitAll().requestMatchers("/eNach/**","/actuator/health").permitAll().anyRequest().authenticated())
+                .authorizeHttpRequests(auth ->auth.requestMatchers("/customer/**").authenticated().requestMatchers("/eNach/**","/actuator/health","/enach-cancel/**").permitAll().anyRequest().authenticated())
 
                 .exceptionHandling(ex -> ex.authenticationEntryPoint(point))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
